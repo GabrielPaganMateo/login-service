24bac00c3359858d8359ab37ab2a1c289a75adc0
GabrielPaganMateo <gabriel.pagan.mateo@gmail.com>
2025-07-24 06:08:57 -0400

    Add profile data

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -17,11 +17,31 @@ function Profile() {
     }
     return (
         <>
-            {user !== undefined ?
-                <div className="profile-container">
-                    <img id="picture" src={user.image} alt="Profile Picture"></img>
-                </div>
-
+            {user !== undefined 
+                ?
+                <>
+                    <div id="profile" className="container">
+                        <div id="identity" className="container">
+                            <div className="container">
+                                <img id="picture" src={user.image} alt="Profile Picture"></img>
+                            </div>
+                            <div className="container">
+                                <h1 id="name">{user.firstName} {user.lastName}</h1>
+                            </div>
+                        </div>
+                        <div id="contact" className="container">
+                            <div><h2>Contact Information:</h2></div>
+                            <div>{user.contact.phone}</div>
+                            <div>{user.contact.email}</div>
+                            <div><h2>Physical Address:</h2></div>
+                            {
+                                Object.entries(user.contact.address).filter(([key]) => key !== "coordinates").map(([key, value]) => {
+                                    return <div key={key}>{value}</div>
+                                })
+                            }
+                        </div>
+                    </div>
+                </>
                 :
                 <Loader/>
             }
--- src/User.js
+++ src/User.js
@@ -4,11 +4,13 @@ class User {
     firstName;
     lastName;
     image;
+    contact;
 
     constructor(response) {
         this.firstName = response.firstName;
         this.lastName = response.lastName;
         this.image = response.image;
+        this.contact = {"phone" : response.phone, "email" : response.email, "address" : response.address}
     }
 }
 
